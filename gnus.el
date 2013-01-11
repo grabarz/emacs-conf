5d1146009dfab53639941620b8246c845ef361c9
Piotr Grabowski <grabarz@gmail.com>
2013-01-11 07:31:46 +0100

    Gnus config update

--- gnus.el
+++ gnus.el
@@ -25,12 +25,31 @@ smtpmail-debug-verb t)
 
 ; http://www.cataclysmicmutation.com/2010/11/multiple-gmail-accounts-in-gnus/#more-12
 (setq gnus-parameters
-      '(("INBOX"
-         (display . all)
-         (posting-style
-         (name "Deon Garrett")
-         (address "me@myworkaddress.com")
-         (organization "My Employer")
-         (signature-file "~/.signature-work"))
-        ; (expiry-target . delete) 
-         (expiry-wait . never))))
+  '(("INBOX"
+    (display . all)
+    (posting-style
+    (name "Piotr Grabowski")
+    (address "grabarz@gmail.com")
+    (organization "MON")
+    (signature-file "~/.emacs.d/signature"))
+   ; (expiry-target . delete) 
+    (expiry-wait . never))))
+
+;(setq gnus-summary-line-format "%U%R%z %(%&user-date;  %-15,15f  %B%s%)\n")
+;(setq gnus-summary-line-forma
+;  "%1{%U%R%z: %}%2{%d%}%5{ %[%4i%] %}%4{%-24,24n%}%6{%-4,4ur%}%5{| %}%(%1{%B%}%s%)n")
+
+;(setq gnus-summary-line-format "%ua%U%R%[%&user-date;: %-15,15f%] %B%s\n")
+
+(when window-system
+(setq
+gnus-sum-thread-tree-root "● "
+gnus-sum-thread-tree-false-root "▷ "
+gnus-sum-thread-tree-single-indent ""
+gnus-sum-thread-tree-leaf-with-other "├─►"
+gnus-sum-thread-tree-vertical "│ "
+gnus-sum-thread-tree-single-leaf "└─►"))
+
+; nowe maile na samej górze
+(setq gnus-thread-sort-functions
+  '(lambda (t1 t2) (not (gnus-thread-sort-by-date t1 t2))))
