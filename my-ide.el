a4ef8480a2848ac13ce79e0fa6229b8f64c3b34a
Piotr <grabarz@gmail.com>
2013-01-24 12:25:27 +0100

    Update my-ide.el

--- my-ide.el
+++ my-ide.el
@@ -3,9 +3,8 @@
 ;
 ; zalozenia:
 ; - dolne okienko na bufory '*...*' pod f-*
-; - boczny panel z nav (f-*);
+; - boczny panel z nav (f-*) albo dired w pojedynczym buforem;
 ; - przeskakiwanie do pliku z bufora obok w nav;
-; - ediff pod nav;
-; - tabbar - zakladki wlaczane i ukrywane pod f;
-; - kilka opcji do konfiguracji okienek i klawiszy;
+; - M-[ i M-] poprzedni i nastepny bufor. Pomija *buferlist*
+: - M-1/2/3/4 aktywowanie okienek w managerze (C-c M-1 - znalezienie biezacego pliku w nav/dired)
 ;
